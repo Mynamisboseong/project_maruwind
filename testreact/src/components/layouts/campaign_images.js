da77fafe64a6bcc46e48bf8797e3d0c674482623
Mynamisboseong <wow456869@gmail.com>
2024-10-23 14:48:45 +0900

    campaign component update

--- testreact/src/components/layouts/campaign_images.js
+++ testreact/src/components/layouts/campaign_images.js
@@ -8,32 +8,34 @@ import './campaign_images.css'; // CSS 파일 가져오기
 function ShapeExample() {
   return (
     <>
-      {/* 제목 컨테이너 */}
-      <Container className="text-center my-4 campaign-title-container">
-        <h2 className="campaign-title">이런 캠페인 공모전은 어떤가요?</h2>
-      </Container>
+      <div className='campaign-background'>
+        {/* 제목 컨테이너 */}
+        <Container className="text-center-campaign-title-container">
+          <span className="campaign-title">이런 캠페인,공모전은 어떤가요?</span>
+        </Container>
 
-      {/* 이미지 컨테이너 */}
-      <Container className="campaign-image-container">
-        <Row>
-          <Col xs={6} md={4}>
-            <Image src="campaign1.png" className="campaign-image" rounded fluid />
-          </Col>
-          <Col xs={6} md={4}>
-            <Image src="campaign2.png" className="campaign-image" rounded fluid />
-          </Col>
-          <Col xs={6} md={4}>
-            <Image src="campaign3.png" className="campaign-image" rounded fluid />
-          </Col>
-        </Row>
-      </Container>
+        {/* 이미지 컨테이너 */}
+        <Container className="campaign-image-container">
+          <Row>
+            <Col xs={6} md={4}>
+              <Image src="campaign1.png" className="campaign-image" rounded fluid />
+            </Col>
+            <Col xs={6} md={4}>
+              <Image src="campaign2.png" className="campaign-image" rounded fluid />
+            </Col>
+            <Col xs={6} md={4}>
+              <Image src="campaign3.png" className="campaign-image" rounded fluid />
+            </Col>
+          </Row>
+        </Container>
 
-      {/* 버튼 컨테이너 */}
-      <Container className="text-center my-4 campaign-button-container">
-        <Button className="campaign-button" size="lg">
-          공모전 캠페인 보러가기
-        </Button>
-      </Container>
+        {/* 버튼 컨테이너 */}
+        <Container className="campaign-button-container">
+          <Button className="campaign-button" size="lg">
+            공모전 캠페인 보러가기
+          </Button>
+        </Container>
+      </div>
     </>
   );
 }
