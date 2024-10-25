4350810b2eb9f7b56b28328c1d83266623893c02
Mynamisboseong <wow456869@gmail.com>
2024-10-25 16:48:00 +0900

    carousels component update

--- testreact/src/components/layouts/carousels.js
+++ testreact/src/components/layouts/carousels.js
@@ -1,62 +1,46 @@
 import Carousel from 'react-bootstrap/Carousel';
-import './carousels.css'
+import './carousels.css';
 
-    function UncontrolledExample() {
+function UncontrolledExample() {
     return (
-        <Carousel style={{width: 'auto', height: '800px', overflow: 'hidden', backgroundColor: '#000a0a'}}>
-        <Carousel.Item style={{ height: '800px' }}>
-            
-            <img
-                    className="d-block w-100"
+        <Carousel className="carousel-container">
+            <Carousel.Item className="carousel-item">
+                <img
+                    className="carousel_slide01"
                     src='eco_slide_01.jpg'
                     alt="First slide"
-                    style={{
-                        objectFit: 'contain',
-                        width: '100%',
-                        height: '100%',
-                        opacity: 0.8
-                    }}
-            />
+                />
+                <div className="carousel-text">
+                    <p>
+                        대한민국 환경보호 웹사이트 <br />
+                        <span className="different-font">마루<span className="highlight">바</span>람</span>
+                    </p>
+                </div>
+            </Carousel.Item>
 
-        <Carousel.Caption className="custom-caption">
-            <p>
-                대한민국 환경보호 웹사이트 <br />
-                <span className="different-font">마루<span className="highlight">바</span>람</span>
-            </p>
-        </Carousel.Caption>
-
-
-        </Carousel.Item>
-        <Carousel.Item style={{ height: '800px' }}>
-            
-            <img
-                    className="d-block w-100"
-                    src='/eco_slide_02.jpg'
+            <Carousel.Item className="carousel-item">
+                <img
+                    className="carousel_slide02"
+                    src='eco_slide_02.jpg'
                     alt="Second slide"
-                    style={{ objectFit: 'contain', width: '100%', height: '100%' }}
                 />
-                
-            <Carousel.Caption className="custom-caption">
-            <p>마루바람 두번째 슬라이드 입니다.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item style={{ height: '800px' }}>
-            
-            <img
-                    className="d-block w-100"
-                    src='/eco_slide_03(2).jpg'
+                <div className="carousel-text">
+                    <p>마루바람 두번째 슬라이드 입니다.</p>
+                </div>
+            </Carousel.Item>
+
+            <Carousel.Item className="carousel-item">
+                <img
+                    className="carousel_slide03"
+                    src='eco_slide_03.jpg'
                     alt="Third slide"
-                    style={{ objectFit: 'contain', width: '100%', height: '100%' }}
                 />
-            
-            <Carousel.Caption className="custom-caption">
-            <p>
-                마루바람 세번째 슬라이드 입니다.
-            </p>
-            </Carousel.Caption>
-        </Carousel.Item>
+                <div className="carousel-text">
+                    <p>마루바람 세번째 슬라이드 입니다.</p>
+                </div>
+            </Carousel.Item>
         </Carousel>
     );
-    }
+}
 
-    export default UncontrolledExample;
+export default UncontrolledExample;
