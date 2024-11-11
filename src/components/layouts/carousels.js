d7ffbe24a0f46b5b2a5f2ac9425837e14cec9157
Mynamisboseong <wow456869@gmail.com>
2024-11-11 19:05:39 +0900

    carousel page 2 update

--- src/components/layouts/carousels.js
+++ src/components/layouts/carousels.js
@@ -1,5 +1,6 @@
-import Carousel from 'react-bootstrap/Carousel';
-import './carousels.css';
+import Carousel from 'react-bootstrap/Carousel'
+import Button from 'react-bootstrap/Button' // 버튼 컴포넌트 추가
+import './carousels.css'
 
 function UncontrolledExample() {
     return (
@@ -7,13 +8,15 @@ function UncontrolledExample() {
             <Carousel.Item className="carousel-item">
                 <img
                     className="carousel_slide01"
-                    src='/eco_slide/eco_slide_01.jpg'
+                    src="/eco_slide/eco_slide_01.jpg"
                     alt="First slide"
                 />
                 <div className="carousel-text">
                     <p>
                         대한민국 환경보호 웹사이트 <br />
-                        <span className="different-font">마루<span className="highlight">바</span>람</span>
+                        <span className="different-font">
+                            마루<span className="highlight">바</span>람
+                        </span>
                     </p>
                 </div>
             </Carousel.Item>
@@ -21,18 +24,27 @@ function UncontrolledExample() {
             <Carousel.Item className="carousel-item">
                 <img
                     className="carousel_slide02"
-                    src='/eco_slide/eco_slide_02.jpg'
+                    src="/eco_slide/eco_slide_02.png"
                     alt="Second slide"
                 />
-                <div className="carousel-text">
-                    <p>마루바람 두번째 슬라이드 입니다.</p>
+                <div className="carousel-contest-text">
+                    <p>
+                        마루바람은{' '}
+                        <span className="contest-hightlight">
+                            환경보호 관련 공모전
+                        </span>
+                        도 추천해준다고?
+                    </p>
                 </div>
+                <Button className="carousel-button" size="lg">
+                            공모전 보러가기
+                </Button>
             </Carousel.Item>
 
             <Carousel.Item className="carousel-item">
                 <img
                     className="carousel_slide03"
-                    src='/eco_slide/eco_slide_03.jpg'
+                    src="/eco_slide/eco_slide_03.jpg"
                     alt="Third slide"
                 />
                 <div className="carousel-text">
@@ -40,7 +52,7 @@ function UncontrolledExample() {
                 </div>
             </Carousel.Item>
         </Carousel>
-    );
+    )
 }
 
-export default UncontrolledExample;
+export default UncontrolledExample
