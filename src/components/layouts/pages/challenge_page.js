f9eae8397bcd708105c23c54a1b09840144a94ee
Mynamisboseong <wow456869@gmail.com>
2024-11-21 14:52:19 +0900

    challengepage banner init

--- src/components/layouts/pages/challenge_page.js
+++ src/components/layouts/pages/challenge_page.js
@@ -1,32 +1,28 @@
-import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
+import './challenge_page.css'
 
 function ChallengePageExample() {
   return (
     <>
-      <div className="banner-container">
-                <Image src="/newspage/news_banner.png" className="banner_image" />
-                <div className='news_banner_text'>
-                    <span className="newsbanner_title_text">
-                        <span className='newbanner_title_hightlight'>환경보호 소식</span>
-                        을 빠르게 확인해보세요
-                    </span><br/>
-                    <span className='newsbanner_subtext'>
-                        환경관련 보도자료와 언론들을 이곳에서 둘러보세요 <br />
-                    </span>
-                    <div className='news_button_overay'>
-                        <Button
-                            className="news_button"
-                            size="lg"
-                            onClick={() =>
-                                (window.location.href = 'https://www.youtube.com/@mevpr/videos')
-                            }
-                        >
-                            환경부 공식유튜브 바로가기
-                        </Button>
-                    </div>
+          <div className="challengepage_banner">
+              <div className='challengePage_banner_smalltitle'>
+                  놀이가 된
+              </div>
+              <div className='challengepage_banner_title'>
+                  친환경 챌린지에 참여해보세요!
+              </div>
+                <div className='challengepage_button_overay'>
+                    <Button
+                        className="news_button"
+                        size="lg"
+                        onClick={() =>
+                            (window.location.href = 'https://www.instagram.com/')
+                        }
+                    >
+                        인스타그램 바로가기
+                    </Button>
                 </div>
-            </div>
+        </div>
     </>
   )
 }
