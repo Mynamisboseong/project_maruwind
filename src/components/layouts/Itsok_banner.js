b93502d3a8a37965124bacc48c2ef9ea0d481042
Mynamisboseong <wow456869@gmail.com>
2024-12-12 18:19:40 +0900

    챌린지 탭 동적화면 수정

--- src/components/layouts/Itsok_banner.js
+++ src/components/layouts/Itsok_banner.js
@@ -1,37 +1,37 @@
-import React, { useEffect, useState, useRef } from 'react'
-import Image from 'react-bootstrap/Image'
-import './itsok_banner.css'
+import React, { useEffect, useState, useRef } from 'react';
+import Image from 'react-bootstrap/Image';
+import './itsok_banner.css';
 
 function FluidExample() {
-    const [animateText, setAnimateText] = useState(false)
-    const bannerRef = useRef(null)
+    const [animateText, setAnimateText] = useState(false);
+    const bannerRef = useRef(null);
 
     useEffect(() => {
         const observerOptions = {
             root: null,
             threshold: 0.5, // 50% 이상 보일 때 애니메이션 시작
-        }
+        };
 
         const observer = new IntersectionObserver((entries) => {
             entries.forEach((entry) => {
                 if (entry.isIntersecting) {
-                    setAnimateText(true) // 애니메이션 시작
+                    setAnimateText(true); // 애니메이션 시작
                 } else {
-                    setAnimateText(false) // 요소가 벗어나면 애니메이션 제거
+                    setAnimateText(false); // 요소가 벗어나면 애니메이션 제거
                 }
-            })
-        }, observerOptions)
+            });
+        }, observerOptions);
 
         if (bannerRef.current) {
-            observer.observe(bannerRef.current)
+            observer.observe(bannerRef.current);
         }
 
         return () => {
             if (bannerRef.current) {
-                observer.unobserve(bannerRef.current)
+                observer.unobserve(bannerRef.current);
             }
-        }
-    }, [])
+        };
+    }, []);
 
     return (
         <div className="itsok_banner_background">
@@ -58,11 +58,11 @@ function FluidExample() {
                     src="eco_distroy.jpg"
                     className="responsive-image"
                     fluid
-                    style={{ width: 'auto', height: 'auto' }}
+                    style={{ width: '100%', height: 'auto' }}
                 />
             </div>
         </div>
-    )
+    );
 }
 
-export default FluidExample
+export default FluidExample;
